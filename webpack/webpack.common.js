8ee806270995083468894f5b039767b2294c8d82
PC-HoangTC <PC-HoangTC@DESKTOP-8F979J0>
2022-04-11 22:35:55 +0700

    update

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
-const { GenerateSW } = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -55,13 +54,6 @@ module.exports = {
                     },
                 },
             ],
-        }),
-        new GenerateSW({
-            navigateFallback: path.resolve(
-                __dirname,
-                '..',
-                './public/index.html'
-            ),
         }),
     ],
     stats: 'errors-only',
