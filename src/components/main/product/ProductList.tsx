3ae97ea6322f75aa4cfe5a1aeab4d9f593a4bbc4
HoangTC <hoang.tran@getz.co>
2022-04-19 18:05:52 +0700

    install antd

--- src/components/main/product/ProductList.tsx
+++ src/components/main/product/ProductList.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ProductList = () => {
     return (
         <div>
             <h1>ProductList</h1>
+
+            {process.env.NODE_ENV}
+            {process.env.PUBLIC_URL}
+            {process.env.TARGET_ENV}
+            <Link to="/product">View Product </Link>
         </div>
     )
 }
